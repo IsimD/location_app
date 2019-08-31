04e6b3ce9a0fcf49f26df4a74c7476c441fc6be1
Patryk Waśko <patryk.wasko@gmail.com>
2019-08-31 16:29:40 +0200

    Setup grape endpoints

---
+++ app/controllers/api/v1/locations.rb
@@ -0,0 +1,14 @@
+module API
+  module V1
+    class Locations < Grape::API
+      include API::V1::Defaults
+
+      resources :locations do
+        desc "Test"
+        get do
+          { message: 'hello' }
+        end
+      end
+    end
+  end
+end
